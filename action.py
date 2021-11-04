b4b9e0ab4cbb558eb3ba322b8fa107f656d05f87
Netanel Revah <netanel@areionsec.com>
2021-11-04 13:14:16 +0200

    refactor and have tests

--- action.py
+++ action.py
@@ -4,6 +4,7 @@ import os
 import sys
 import time
 import uuid
+from dataclasses import dataclass
 from functools import cached_property
 from urllib.request import Request, urlopen
 
@@ -73,18 +74,41 @@ class Worker:
 
 
 class GitHubSecretScanner(Worker):
-    def __init__(self, webhook_url, github_details, action_unique_id, timeout_seconds: float, delay_seconds: float):
-        super().__init__(timeout_seconds, delay_seconds)
-        self.webhook_url = webhook_url
-        self._github_details = github_details
-        self.action_unique_id = action_unique_id
+    def __init__(self, context, delay_seconds: float):
+        super().__init__(context.input_timeout_seconds, delay_seconds)
+        self.context = context
 
     @cached_property
     def github_details(self):
-        return dict(self._github_details, action_unique_id=self.action_unique_id)
+        return {
+            "action_unique_id": self.context.action_unique_id,
+            "github_run_id": self.context.github_run_id,
+            "github_run_number": self.context.github_run_number,
+            "github_head_ref": self.context.github_head_ref,
+            "github_base_ref": self.context.github_base_ref,
+            "github_token": self.context.github_token,
+            "github_sha": self.context.github_sha,
+            "github_repository": self.context.github_repository,
+        }
+
+    @classmethod
+    def print_secrets(cls, details):
+        for secret in details:
+            if secret["is_valid"] in ["IS_SECRET_VALID_UNSPECIFIED", "IS_SECRET_VALID_NO"]:
+                print(
+                    f"::notice file={secret['file_path']}::found invalid secret {secret['secret_value']} of {secret['secret_type']}"
+                )
+            elif secret["is_valid"] in ["IS_SECRET_VALID_YES"]:
+                print(
+                    f"::error file={secret['file_path']}::found valid secret {secret['secret_value']} of {secret['secret_type']}"
+                )
+            else:
+                print(
+                    f"::warning file={secret['file_path']}::found unknown secret {secret['secret_value']} of {secret['secret_type']}"
+                )
 
     def execute(self):
-        response = requests.post(self.webhook_url, json=self.github_details)
+        response = requests.post(self.context.input_webhook_url, json=self.github_details)
 
         if response.status_code != 200:
             raise Exception(f"webhook returned invalid status code: {response.status_code}")
@@ -95,22 +119,11 @@ class GitHubSecretScanner(Worker):
             raise Exception(f"webhook failed with error: {result['error']}")
 
         if result["status"] == "failed":
+            self.print_secrets(result.get("details", []))
             raise ValidSecretsException()
 
         if result["status"] != "just_created":
-            for secret in result.get("details", []):
-                if secret["is_valid"] in ["IS_SECRET_VALID_UNSPECIFIED", "IS_SECRET_VALID_NO"]:
-                    print(
-                        f"::notice file={secret['file_path']}::found invalid secret {secret['secret_value']} of {secret['secret_type']}"
-                    )
-                elif secret["is_valid"] in ["IS_SECRET_VALID_YES"]:
-                    print(
-                        f"::error file={secret['file_path']}::found valid secret {secret['secret_value']} of {secret['secret_type']}"
-                    )
-                else:
-                    print(
-                        f"::warning file={secret['file_path']}::found unknown secret {secret['secret_value']} of {secret['secret_type']}"
-                    )
+            self.print_secrets(result.get("details", []))
             return
 
         raise Delay()
@@ -118,38 +131,49 @@ class GitHubSecretScanner(Worker):
     @classmethod
     def create(
         cls,
-        webhook_url,
-        github_details,
-        action_unique_id=None,
-        timeout_seconds=DEFAULT_TIMEOUT_SECONDS,
+        context: "Context",
         delay_seconds=DEFAULT_DELAY_SECONDS,
     ):
-        action_unique_id = action_unique_id or uuid.uuid4().hex
-        return cls(webhook_url, github_details, action_unique_id, timeout_seconds, delay_seconds)
+        return cls(context, delay_seconds)
 
 
-def collect_details():
-    with open("/github/workflow/event.json", "r") as event_file:
-        event = json.load(event_file)
+@dataclass
+class Context:
+    action_unique_id: str
+    github_run_id: str
+    github_run_number: str
+    github_head_ref: str
+    github_base_ref: str
+    github_token: str
+    github_sha: str
+    github_repository: str
+    input_webhook_url: str
+    input_timeout_seconds: float
 
-    return {
-        "github_repository": event["repository"]["full_name"],
-        "github_run_id": os.getenv("GITHUB_RUN_ID"),
-        "github_run_number": os.getenv("GITHUB_RUN_NUMBER"),
-        "github_sha": event["pull_request"]["head"]["sha"],
-        "github_head_ref": os.getenv("GITHUB_HEAD_REF"),
-        "github_base_ref": os.getenv("GITHUB_BASE_REF"),
-        "github_token": os.getenv("INPUT_TOKEN"),
-    }
+    @classmethod
+    def create(cls):
+        with open(os.getenv("GITHUB_EVENT_PATH"), "r") as event_file:
+            event = json.load(event_file)
+
+        return cls(
+            action_unique_id=uuid.uuid4().hex,
+            github_run_id=os.getenv("GITHUB_RUN_ID"),
+            github_run_number=os.getenv("GITHUB_RUN_NUMBER"),
+            github_head_ref=os.getenv("GITHUB_HEAD_REF"),
+            github_base_ref=os.getenv("GITHUB_BASE_REF"),
+            github_token=os.getenv("INPUT_TOKEN"),
+            input_webhook_url=os.getenv("INPUT_WEBHOOK_URL"),
+            github_repository=event["repository"]["full_name"],
+            github_sha=event["pull_request"]["head"]["sha"],
+            input_timeout_seconds=float(os.getenv("INPUT_TIMEOUT_SECONDS", DEFAULT_TIMEOUT_SECONDS)),
+        )
 
 
 # noinspection PyBroadException
 def main():
-    details = collect_details()
     try:
-        input_webhook_url = os.getenv("INPUT_WEBHOOK_URL")
-        input_timeout_seconds = float(os.getenv("INPUT_TIMEOUT_SECONDS", DEFAULT_TIMEOUT_SECONDS))
-        GitHubSecretScanner.create(input_webhook_url, details, timeout_seconds=input_timeout_seconds).run()
+        context = Context.create()
+        GitHubSecretScanner.create(context).run()
         print("::notice::no secrets found \U0001f44d")
     except TimeoutError:
         print("::error::action timeout!")
